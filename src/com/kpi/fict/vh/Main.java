b33726c051fa2d127aed522a6b0e8397184214a5
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-09-07 12:11:27 +0300

    method f NullPointerException

--- src/com/kpi/fict/vh/Main.java
+++ src/com/kpi/fict/vh/Main.java
@@ -3,16 +3,28 @@ package com.kpi.fict.vh;
 public class Main {
 
     public static void main(String[] args) {
-        catchExceptionWithTryCatch();
+//        catchExceptionWithTryCatch();
+        f(null); // StackOverflowError
     }
 
     public static void trowUpException() throws Throwable {
+        Error errorThrow = new Error();
+        throw errorThrow;
+//        throw null; NullPointerException
     }
 
     public static void catchExceptionWithTryCatch(){
         try {
             trowUpException();
         } catch (Throwable c){
+        }
+    }
+
+    public static void f(NullPointerException e) {
+        try {
+            throw e;
+        } catch (NullPointerException npe) {
+            f(npe);
         }
     }
 }
