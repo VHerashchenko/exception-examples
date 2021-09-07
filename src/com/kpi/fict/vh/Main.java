18d5995e435b6aa4483cc29554abecfe882dc508
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-09-07 12:04:33 +0300

    method throws and try carch

--- src/com/kpi/fict/vh/Main.java
+++ src/com/kpi/fict/vh/Main.java
@@ -3,6 +3,12 @@ package com.kpi.fict.vh;
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            trowUpException();
+        } catch (Throwable c){
+        }
+    }
 
+    public static void trowUpException() throws Throwable {
     }
 }
