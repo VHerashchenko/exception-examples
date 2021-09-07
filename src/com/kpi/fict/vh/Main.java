fe13268a968d2e7dfb08a208c614c65ae3944701
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-09-07 12:06:46 +0300

    method catchException

--- src/com/kpi/fict/vh/Main.java
+++ src/com/kpi/fict/vh/Main.java
@@ -3,12 +3,16 @@ package com.kpi.fict.vh;
 public class Main {
 
     public static void main(String[] args) {
+        catchExceptionWithTryCatch();
+    }
+
+    public static void trowUpException() throws Throwable {
+    }
+
+    public static void catchExceptionWithTryCatch(){
         try {
             trowUpException();
         } catch (Throwable c){
         }
-    }
-
-    public static void trowUpException() throws Throwable {
     }
 }
