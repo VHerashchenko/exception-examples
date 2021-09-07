44ac0dbf6562cddd0389afbf760e5a6596690dac
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-09-07 13:43:08 +0300

    NonLocalControlTransfer

--- src/com/kpi/fict/vh/Main.java
+++ src/com/kpi/fict/vh/Main.java
@@ -1,6 +1,7 @@
 package com.kpi.fict.vh;
 
 import com.kpi.fict.vh.exception.FirstException;
+import com.kpi.fict.vh.exception.NonlocalControlTransfer;
 import com.kpi.fict.vh.exception.ReturnConversion;
 
 public class Main {
@@ -8,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         FirstException fExc = new FirstException();
         ReturnConversion retConv = new ReturnConversion();
+        NonlocalControlTransfer nonlocalCT = new NonlocalControlTransfer();
 
         fExc.catchExceptionWithTryCatch();
 //        fExc.f(null); // StackOverflowError recursion
@@ -18,6 +20,9 @@ public class Main {
 //        retConv.sqrIfOperator(9); //while(true);
 //        retConv.sqrExceptionThrow(10); // RUNTIME ERROR: Exception in thread "main" java.lang.RuntimeException
 
-        retConv.area(-1, -1);
+//        retConv.area(-1, -1);
+
+//        nonlocalCT.nonCatchException();
+        nonlocalCT.catchException();
     }
 }
---
+++ src/com/kpi/fict/vh/exception/NonlocalControlTransfer.java
@@ -0,0 +1,42 @@
+package com.kpi.fict.vh.exception;
+
+public class NonlocalControlTransfer {
+
+    public void catchException(){
+        System.err.println("#1.in");
+        try {
+            f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        } catch (Error e) { // "перехватили" "летящее" исключение
+            System.err.println("#1.CATCH");  // и работаем
+        }
+        System.err.println("#1.out");  // работаем дальше
+    }
+
+    public void nonCatchException(){
+        System.err.println("#1.in");
+        f();
+        System.err.println("#1.out");
+    }
+
+    private void f() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out");  //вернулись
+    } // выходим из текущего фрейма, кончились инструкции
+
+    private void g() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // вернулись
+    } // выходим из текущего фрейма, кончились инструкции
+
+    private void h() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.RETURN");
+            throw new Error();
+//            return; // выходим из текущего фрейма по 'return'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСКАЕМ
+    }
+}
